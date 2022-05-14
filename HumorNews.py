121670fd1b75c00d760d8293d65f2a6c092998aa
Tolu <95661346+tolu-joel@users.noreply.github.com>
2022-05-14 18:27:00 +0100

    CODE REFACTOR & TEMPLATE ADDED
    #updates #bugs
    
    1) Added two new post sources.
    
    2) created function to automate parsing of posts.
    
    3) The two new post source(webpage_1, webpage_2) doesn't return href properly.

--- HumorNews.py
+++ HumorNews.py
@@ -4,41 +4,50 @@ from bs4 import BeautifulSoup
 
 all_posts = [[], []]
 
+
+def parse(post_source):
+    for post in post_source:
+        post_title = post.text
+        if "https" not in (post_link := post.a['href']):
+            post_link = 'https://www.ladbible.com' + post_link
+        all_posts[0].append(post_title)
+        all_posts[1].append(post_link)
+
+
 web_page_1 = requests.get('https://www.today.com/news/good-news')
 soup = BeautifulSoup(web_page_1.text, 'lxml')
 posts_source_1 = soup.find_all('h2', class_='tease-card__headline tease-card__title tease-card__title--today relative')
-for post in posts_source_1:
-    post_title = post.text
-    post_link = post.a['href']
-    all_posts[0].append(post_title)
-    all_posts[1].append(post_link)
+parse(posts_source_1)
 
 web_page_2 = requests.get('https://www.goodnewsnetwork.org/category/news/inspiring/')
 soup = BeautifulSoup(web_page_2.text, 'lxml')
 posts_source_2 = soup.find_all('h3', class_='entry-title td-module-title')
-for post in posts_source_2:
-    if 'EPISODE' not in post.a.text:
-        post_title = post.text
-        post_link = post.a['href']
-        all_posts[0].append(post_title)
-        all_posts[1].append(post_link)
+parse(posts_source_2)
 
+web_page_3 = requests.get('https://www.ladbible.com/weird')
+soup = BeautifulSoup(web_page_3.text, 'lxml')
+posts_source_3 = soup.find_all('div', class_='css-32xpko-cardText-padding-padding-padding-padding-padding-padding-padding')
+parse(posts_source_3)
+
+web_page_4 = requests.get('https://www.theweek.co.uk/odd-news-0')
+soup = BeautifulSoup(web_page_4.text, 'lxml')
+posts_source_4 = soup.find_all('div', class_='polaris__article-card -layout-default -default polaris__article-group--single')
+parse(posts_source_4)
+
+all_posts = set(zip(all_posts[0], all_posts[1]))  # randomize posts
 html = ''
 
-for index, (title, link) in enumerate(zip(all_posts[0], all_posts[1])):
-    print(index, link, title)
+for index, (title, link) in enumerate(all_posts):
     html_title = f'<h2><a href="{link}">{(index + 1)}. {title}</a></h2>'
     html += html_title
 
-html = f'''<h1>The Humor News 🙈</h1>
-<h2>By: Tolu Joel</h2>'
-{html}
-<p><span>Scraped with love by Tolu Joel...hehe ❤️</span>
-<span><a href="https://github.com/tolu-joel">Github</a></span>
-<span><a href="https://linkedin.com/in/tolujoel">LinkedIn</a></span>
-<span><a href="https://twitter.com/tolu_joel_">Twitter</a></span>
-<span><a href="https://instagram.com/tolu_joel_">Instagram</a></span></p>
-'''
-# print(html)
-email = yagmail.SMTP('your@gmail.com', input('Email Password: '))
-email.send('receiver@gmail.com', 'The Humor News', html)
+with open('template.html', 'r') as template:
+    data = template.readlines()
+    data[65] = html + '\n'  # passing the html posts to the template
+
+with open('template.html', 'w') as template:
+    template.writelines(data)
+
+with open('template.html', 'r') as template:
+    email = yagmail.SMTP('your@gmail.com', input('Email Password: '))
+    email.send('receiver@gmail.com', 'The Humor News', template)
